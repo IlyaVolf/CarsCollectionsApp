d0d6811c421fe4830d4b46f0fa1b09ea5be24c18
Ilya Volf <volfilya@gmail.com>
2023-09-01 02:13:01 +0700

    CarDetailsScreen: small visual fixes, support of EditScreen navigation has been added

--- app/src/main/java/com/example/carscollectionsapp/presentation/car_details_screen/CarDetailesScreen.kt
+++ app/src/main/java/com/example/carscollectionsapp/presentation/car_details_screen/CarDetailesScreen.kt
@@ -1,33 +1,18 @@
 package com.example.carscollectionsapp.presentation.car_details_screen
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.example.carscollectionsapp.R
 import com.example.carscollectionsapp.presentation.car_details_screen.entities.CarDetailsScreenEffect
 import com.example.carscollectionsapp.presentation.car_details_screen.entities.CarDetailsScreenEvent
 import com.example.carscollectionsapp.presentation.car_details_screen.entities.CarDetailsScreenState
 import com.example.carscollectionsapp.presentation.car_details_screen.views.CarDetailsScreenLoading
 import com.example.carscollectionsapp.presentation.car_details_screen.views.CarDetailsScreenSuccessful
+import com.example.carscollectionsapp.presentation.navigation.CarsAppScreens
 import com.example.carscollectionsapp.utils.collectAsEffect
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CarDetailsScreen(
     carId: Long,
@@ -44,45 +29,24 @@ fun CarDetailsScreen(
     viewModel.effect.collectAsEffect { effect ->
         when (effect) {
             is CarDetailsScreenEffect.NavigateToCarEditScreen -> {
-               /* navController.navigate(
-                    CarsAppScreens.CarAddEditScreen.passArguments(effect.carId)
-                )*/
+                navController.navigate(
+                    CarsAppScreens.CarEditScreen.passArguments(effect.carId)
+                )
             }
-
-            else -> {}
         }
     }
 
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { viewModel.onEvent(CarDetailsScreenEvent.OnEditClicked) },
-                shape = CircleShape,
-                containerColor = MaterialTheme.colorScheme.primary,
-                contentColor = MaterialTheme.colorScheme.background,
-                modifier = Modifier.size(80.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Edit,
-                    contentDescription = stringResource(R.string.description)
-                )
+    when (state.value) {
+        is CarDetailsScreenState.Loading -> CarDetailsScreenLoading()
+
+        is CarDetailsScreenState.Successful -> CarDetailsScreenSuccessful(
+            car = (state.value as CarDetailsScreenState.Successful).car,
+            onAction = { event ->
+                viewModel.onEvent(event)
             }
-        },
-        floatingActionButtonPosition = FabPosition.End
-    ) { paddingValues ->
+        )
 
-        when (state.value) {
-            is CarDetailsScreenState.Loading -> CarDetailsScreenLoading(
-                modifier = Modifier.padding(paddingValues = paddingValues)
-            )
-
-            is CarDetailsScreenState.Successful -> CarDetailsScreenSuccessful(
-                car = (state.value as CarDetailsScreenState.Successful).car,
-                modifier = Modifier.padding(paddingValues = paddingValues)
-            )
-
-            else -> {}
-        }
+        else -> {}
 
     }
 
--- app/src/main/java/com/example/carscollectionsapp/presentation/car_details_screen/views/CarDetailesScreenSuccessful.kt
+++ app/src/main/java/com/example/carscollectionsapp/presentation/car_details_screen/views/CarDetailesScreenSuccessful.kt
@@ -7,9 +7,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,101 +31,122 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.carscollectionsapp.R
 import com.example.carscollectionsapp.domain.entities.Car
+import com.example.carscollectionsapp.presentation.car_details_screen.entities.CarDetailsScreenEvent
 import com.example.carscollectionsapp.presentation.customView.CustomImage
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CarDetailsScreenSuccessful(
     car: Car,
     modifier: Modifier = Modifier,
-    onCarClicked: (Long) -> Unit = {},
+    onAction: (CarDetailsScreenEvent) -> Unit,
 ) {
-    Box(modifier = modifier) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(color = MaterialTheme.colorScheme.background),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
 
-            CustomImage(
-                uri = car.photo,
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { onAction(CarDetailsScreenEvent.OnEditClicked) },
+                shape = CircleShape,
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.background,
+                modifier = Modifier.size(80.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.description)
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
+    ) { paddingValues ->
+        Box(modifier = modifier.padding(paddingValues = paddingValues)) {
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .aspectRatio(1f)
-            )
-
-            Column(
-                modifier = Modifier.padding(horizontal = 16.dp)
+                    .background(color = MaterialTheme.colorScheme.background),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
 
-                Text(
-                    text = car.name,
+                CustomImage(
+                    uri = car.photo,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 16.dp),
-                    style = MaterialTheme.typography.titleLarge,
-                    fontSize = 24.sp,
-                    textAlign = TextAlign.Center
+                        .aspectRatio(1f)
                 )
 
-
-                Row(
-                    modifier = Modifier
-                        .align(alignment = Alignment.CenterHorizontally)
-                        .padding(8.dp)
+                Column(
+                    modifier = Modifier.padding(horizontal = 16.dp)
                 ) {
 
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.width(80.dp)
+                    Text(
+                        text = car.name,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp),
+                        style = MaterialTheme.typography.titleLarge,
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier
+                            .align(alignment = Alignment.CenterHorizontally)
                     ) {
-                        Text(
-                            text = car.year.toString(),
-                            fontSize = 21.sp,
-                            modifier = Modifier
-                                .padding(vertical = 8.dp),
-                            style = MaterialTheme.typography.titleSmall,
-                            textAlign = TextAlign.Center
-                        )
 
-                        Spacer(modifier = Modifier.width(4.dp))
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.width(80.dp)
+                        ) {
+                            Text(
+                                text = car.year.toString(),
+                                fontSize = 21.sp,
+                                modifier = Modifier
+                                    .padding(vertical = 8.dp),
+                                style = MaterialTheme.typography.titleSmall,
+                                textAlign = TextAlign.Center
+                            )
 
-                        Text(
-                            text = stringResource(R.string.production_year),
-                            modifier = Modifier
-                                .padding(vertical = 8.dp),
-                            style = MaterialTheme.typography.bodySmall,
-                            textAlign = TextAlign.Center,
-                            color = MaterialTheme.colorScheme.primary
-                        )
+                            Spacer(modifier = Modifier.width(4.dp))
 
-                    }
+                            Text(
+                                text = stringResource(R.string.production_year),
+                                modifier = Modifier
+                                    .padding(vertical = 8.dp),
+                                style = MaterialTheme.typography.bodySmall,
+                                textAlign = TextAlign.Center,
+                                color = MaterialTheme.colorScheme.primary
+                            )
 
-                    Spacer(modifier = Modifier.width(12.dp))
+                        }
 
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.width(80.dp)
-                    ) {
-                        Text(
-                            fontSize = 21.sp,
-                            text = car.engineCapacity.toString(),
-                            modifier = Modifier
-                                .padding(vertical = 8.dp),
-                            style = MaterialTheme.typography.titleSmall,
-                            textAlign = TextAlign.Center
-                        )
+                        Spacer(modifier = Modifier.width(12.dp))
 
-                        Spacer(modifier = Modifier.width(4.dp))
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.width(80.dp)
+                        ) {
+                            Text(
+                                fontSize = 21.sp,
+                                text = car.engineCapacity.toString(),
+                                modifier = Modifier
+                                    .padding(vertical = 8.dp),
+                                style = MaterialTheme.typography.titleSmall,
+                                textAlign = TextAlign.Center
+                            )
 
-                        Text(
-                            text = stringResource(R.string.engine_capacity),
-                            modifier = Modifier
-                                .padding(vertical = 8.dp),
-                            style = MaterialTheme.typography.bodySmall,
-                            textAlign = TextAlign.Center,
-                            color = MaterialTheme.colorScheme.primary
-                        )
+                            Spacer(modifier = Modifier.width(4.dp))
+
+                            Text(
+                                text = stringResource(R.string.engine_capacity),
+                                modifier = Modifier
+                                    .padding(vertical = 8.dp),
+                                style = MaterialTheme.typography.bodySmall,
+                                textAlign = TextAlign.Center,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        }
                     }
                 }
             }
@@ -126,7 +157,7 @@ fun CarDetailsScreenSuccessful(
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun test() {
-    CarDetailsScreenSuccessful(
+    /*CarDetailsScreenSuccessful(
         Car(1, "Lada Vesta", null, 2019, 1.6F, 0L)
-    )
+    )*/
 }
