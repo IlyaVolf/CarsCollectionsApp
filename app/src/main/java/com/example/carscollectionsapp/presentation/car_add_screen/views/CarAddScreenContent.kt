076d3bbe20fdfc69e5ba7d1fb001e79b59092ef5
Ilya Volf <volfilya@gmail.com>
2023-08-31 22:49:00 +0700

    CarAddScreenSuccessful -> CarAddScreenContent

--- app/src/main/java/com/example/carscollectionsapp/presentation/car_add_screen/views/CarAddScreenContent.kt
+++ app/src/main/java/com/example/carscollectionsapp/presentation/car_add_screen/views/CarAddScreenContent.kt
@@ -56,6 +56,7 @@ import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.resolveDefaults
 import com.example.carscollectionsapp.presentation.car_add_screen.entities.CarAddScreenEvent
@@ -72,79 +73,100 @@ fun CarAddScreenSuccessful(
 
     val scrollState = rememberScrollState()
     val launcher = rememberLauncherForActivityResult(PickVisualMedia()) {
-        //component.updatePhoto(it)
+        onAction(CarAddScreenEvent.OnPhotoChanged(it.toString()))
     }
 
     Column(
         modifier = Modifier
-            .verticalScroll(scrollState)
             .padding(
                 horizontal = 16.dp,
             )
-            .fillMaxSize(),
+            .fillMaxSize()
     ) {
-        /*CustomImage(
-            uri = carAddContainer.photoString,
+        Column(
             modifier = Modifier
-                .fillMaxWidth(0.7f)
-                .aspectRatio(1f)
-                .align(Alignment.CenterHorizontally)
-                .clip(RoundedCornerShape(54.dp)),
-            onClick = {
-                launcher.launch(
-                    PickVisualMediaRequest(
-                        mediaType = PickVisualMedia.ImageOnly
-                    )
-                )
-            }
-        )*/
-        CustomTextField(
-            label = stringResource(R.string.car_name),
-            isNecessaryField = true,
-            value = carAddContainer.nameString,
-            singleLine = true,
-            onValueChange = { onAction(CarAddScreenEvent.OnNameChanged(it)) },
-            state = carAddContainer.nameState,
-            modifier = Modifier
-                .fillMaxWidth(),
-        )
-        CustomTextField(
-            label = stringResource(R.string.production_year),
-            isNecessaryField = true,
-            value = carAddContainer.yearString,
-            singleLine = true,
-            onValueChange = { onAction(CarAddScreenEvent.OnYearChanged(it)) },
-            keyboardType = KeyboardType.Number,
-            state = carAddContainer.yearState,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
-        CustomTextField(
-            label = stringResource(R.string.engine_capacity),
-            isNecessaryField = true,
-            value = carAddContainer.engineCapacityString,
-            singleLine = true,
-            onValueChange = { onAction(CarAddScreenEvent.OnEngineCapacityChanged(it)) },
-            keyboardType = KeyboardType.Decimal,
-            state = carAddContainer.engineCapacityState,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
+                .verticalScroll(scrollState)
+                .weight(1f)
+        ) {
+            CustomImage(
+                uri = carAddContainer.photoString,
+                modifier = Modifier
+                    .padding(vertical = 16.dp)
+                    .fillMaxWidth(0.7f)
+                    .aspectRatio(1f)
+                    .align(Alignment.CenterHorizontally)
+                    .clip(RoundedCornerShape(54.dp))
+                    .clickable {
+                        launcher.launch(
+                            PickVisualMediaRequest(
+                                mediaType = PickVisualMedia.ImageOnly
+                            )
+                        )
+                    },
+            )
+            CustomTextField(
+                label = stringResource(R.string.car_name),
+                isNecessaryField = true,
+                value = carAddContainer.nameString,
+                singleLine = true,
+                onValueChange = { onAction(CarAddScreenEvent.OnNameChanged(it)) },
+                state = carAddContainer.nameState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                imeAction = ImeAction.Next
+            )
+            CustomTextField(
+                label = stringResource(R.string.production_year),
+                isNecessaryField = true,
+                value = carAddContainer.yearString,
+                singleLine = true,
+                onValueChange = { onAction(CarAddScreenEvent.OnYearChanged(it)) },
+                keyboardType = KeyboardType.Number,
+                state = carAddContainer.yearState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                imeAction = ImeAction.Next
+            )
+            CustomTextField(
+                label = stringResource(R.string.engine_capacity),
+                isNecessaryField = true,
+                value = carAddContainer.engineCapacityString,
+                singleLine = true,
+                onValueChange = { onAction(CarAddScreenEvent.OnEngineCapacityChanged(it)) },
+                keyboardType = KeyboardType.Decimal,
+                state = carAddContainer.engineCapacityState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                imeAction = ImeAction.Go
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+        }
 
         CustomButton(
             text = "Save",
             onClick = {
                 onAction(CarAddScreenEvent.OnSaveClicked)
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
         )
 
         CustomButton(
             text = "Cancel",
             outlined = true,
             onClick = { onAction(CarAddScreenEvent.OnCancelClicked) },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
         )
+
+        Spacer(modifier = Modifier.height(12.dp))
     }
 
 }
