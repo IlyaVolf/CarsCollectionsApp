936f2f3a5116f94263a4e89ccb3595a1ede2654f
Ilya Volf <volfilya@gmail.com>
2023-08-31 22:46:56 +0700

    Main Screen has been redesigned

--- app/src/main/java/com/example/carscollectionsapp/presentation/main_screen/views/MainScreenSuccessful.kt
+++ app/src/main/java/com/example/carscollectionsapp/presentation/main_screen/views/MainScreenSuccessful.kt
@@ -1,12 +1,13 @@
 package com.example.carscollectionsapp.presentation.main_screen.views
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import com.example.carscollectionsapp.domain.entities.Car
 
@@ -16,11 +17,17 @@ fun MainScreenSuccessfulState(
     modifier: Modifier = Modifier,
     onCarClicked: (Long) -> Unit = {},
 ) {
-    LazyVerticalGrid(columns = GridCells.Fixed(1), modifier = modifier) {
+
+    val configuration = LocalConfiguration.current
+    val gridCellsCount = when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> 2
+        else -> 1
+    }
+
+    LazyVerticalGrid(columns = GridCells.Fixed(gridCellsCount), modifier = modifier.padding(horizontal = 4.dp)) {
         items(cars, key = { it.id }) {
             CarItem(
                 car = it,
-                modifier = Modifier.padding(vertical = 8.dp),
                 onCarClicked = onCarClicked
             )
         }
