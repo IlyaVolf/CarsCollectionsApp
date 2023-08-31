930beb0e81aed9f4acc8b725f438da1fb6281daf
Ilya Volf <volfilya@gmail.com>
2023-08-31 22:46:27 +0700

    CarItem has been redesigned

--- app/src/main/java/com/example/carscollectionsapp/presentation/main_screen/views/CarItem.kt
+++ app/src/main/java/com/example/carscollectionsapp/presentation/main_screen/views/CarItem.kt
@@ -3,32 +3,28 @@ package com.example.carscollectionsapp.presentation.main_screen.views
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.Icon
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.carscollectionsapp.R
+import androidx.compose.ui.unit.sp
 import com.example.carscollectionsapp.domain.entities.Car
-import com.skydoves.landscapist.coil.CoilImage
-import com.skydoves.landscapist.glide.GlideImage
+import com.example.carscollectionsapp.presentation.customView.CustomImage
 
 @Composable
 fun CarItem(
@@ -40,34 +36,48 @@ fun CarItem(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp)
-            .background(color = MaterialTheme.colorScheme.background)
-            .clickable {
-                onCarClicked(car.id)
-            },
-        shape = RoundedCornerShape(16.dp),
+            .padding(4.dp)
+            .background(color = MaterialTheme.colorScheme.background),
+        shape = RoundedCornerShape(24.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        )
     ) {
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            GlideImage(
-                imageModel = { R.drawable.image_placeholder },
-                modifier = Modifier
-                    .clip(MaterialTheme.shapes.extraLarge)
-                    .size(180.dp)
-                    .padding(4.dp),
-                previewPlaceholder = R.drawable.image_placeholder,
+        Box(
+            modifier = Modifier
+            .clickable {
+            onCarClicked(car.id)
+        },
+        ) {
 
-            )
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                CustomImage(
+                    uri = car.photo,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1.33f)
+                        .padding(4.dp)
+                        .clip(RoundedCornerShape(24.dp)),
+                )
 
-            Text(
-                text = car.name,
-                style = MaterialTheme.typography.titleMedium,
-                color = MaterialTheme.colorScheme.primary,
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 16.dp),
-                maxLines = 1,
-            )
+                Spacer(modifier = Modifier.height(8.dp))
 
+                Text(
+                    text = car.name,
+                    style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center,
+                    fontSize = 14.sp,
+                    color = MaterialTheme.colorScheme.primary,
+                    maxLines = 1,
+                    overflow = TextOverflow.Clip,
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp),
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+            }
         }
     }
 }
@@ -75,5 +85,5 @@ fun CarItem(
 @Preview
 @Composable
 fun MyViewPreview() {
-    CarItem(car = Car(0, "abcdefghjklmnopqrstuvwnz123456789", "https://www.protoolreviews.com/wp-content/uploads/2017/08/Hilti-SF-6H-A22-22V-Hammer-Drill-01-650x434.jpg", 1999, 1.4F, 11L))
+    CarItem(car = Car(0, "abcdefghjklmnopqrstuvwnz1fsssssssssssssssssssssssssss23456789", "", 1999, 1.4F, 11L))
 }
